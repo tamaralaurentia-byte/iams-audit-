53e61820be37a07e003bfc6e99a912d829b0d716
tamaralaurentia-byte <tamaralaurentia@gmail.com>
2026-06-06 19:23:02 +0700

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,1 +1,18 @@
+-- Membuat tabel Users
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    full_name VARCHAR(100) NOT NULL,
+    role VARCHAR(20) NOT NULL -- 'Auditor' atau 'Auditee'
+);
 
+-- Membuat tabel Findings (Temuan)
+CREATE TABLE findings (
+    id SERIAL PRIMARY KEY,
+    category VARCHAR(100) NOT NULL, -- Selisih stock, Lapping, Fraud, dll
+    description TEXT NOT NULL,
+    risk_level VARCHAR(20),
+    status VARCHAR(20) DEFAULT 'Open',
+    due_date DATE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
